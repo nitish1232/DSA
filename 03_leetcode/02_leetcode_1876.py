ea01217f50c9f28e628f80989719bb5b1b586c0d
Nitish Ugru <nitish.ugru@nutanix.com>
2025-08-29 16:05:48 +0530

    Leetcode (#2)
    * Adding 2 sliding window solutions
    
    * Much faster solution added
    
    ---------
    
    Co-authored-by: nitish1232 <nitish1232@gmail.com>

--- 03_leetcode/02_leetcode_1876.py
+++ 03_leetcode/02_leetcode_1876.py
@@ -9,8 +9,30 @@ def substring(s:str) -> int:
             count += 1
     return count
 
+
+# length function, creation of set, slicing will take more memory and time.
+# Hence, always try to use primitive data structures.
+# Above function took 3ms in leetcode.
+# In each window of size 3, compare each characters one by one directly.
+# Below solution took almost 0ms in leetcode.
+def substring_faster(s:str) -> int:
+    count = 0
+    for i in range(len(s)-2):
+        a = s[i]
+        b = s[i+1]
+        c = s[i+2]
+        if a != b and b != c and a != c:
+            count += 1
+    return count
+
 print(substring("xyzzaz"))
 print(substring("aababcabc"))
 print(substring("abc"))
 print(substring("ab"))
 print(substring("abb"))
+
+print(substring_faster("xyzzaz"))
+print(substring_faster("aababcabc"))
+print(substring_faster("abc"))
+print(substring_faster("ab"))
+print(substring_faster("abb"))
